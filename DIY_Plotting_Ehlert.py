a21f381eb43023df24fdd73c2f54dd7bdb433dc0
Tony Ehlert <aehlert@dmacc.edu>
2023-10-02 11:45:05 -0500

    Formatted

--- DIY_Plotting_Ehlert.py
+++ DIY_Plotting_Ehlert.py
@@ -13,15 +13,16 @@ import matplotlib.pyplot as plt
 
 # read .csv file to new df
 base_video_game_df = pd.read_csv('Video_Games_Dataset.csv')
-#print(base_video_game_df)
+# print(base_video_game_df)
 
 # create new df with only needed columns
-vid_game_sales_by_genre_df = base_video_game_df[['Genre', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales']].copy()
-#print(vid_game_sales_by_genre_df)
+vid_game_sales_by_genre_df = base_video_game_df[
+    ['Genre', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales']].copy()
+# print(vid_game_sales_by_genre_df)
 
 # remove any records that has null/NaN for 'Genre'
 vid_game_sales_df = vid_game_sales_by_genre_df.dropna(subset=['Genre'])
-#print(vid_game_sales_df)
+# print(vid_game_sales_df)
 
 # create new grouped by genre df
 vid_game_sales_by_genre_df = vid_game_sales_by_genre_df.groupby(['Genre'])
@@ -31,25 +32,25 @@ vid_game_sales_by_genre_df = vid_game_sales_by_genre_df.sum()
 
 # sort grouped df by 'Global_Sales' in ascending order
 vid_game_sales_by_genre_df = vid_game_sales_by_genre_df.sort_values(by=['Global_Sales'], ascending=True)
-#print(vid_game_sales_by_genre_df.to_string())
-#print(vid_game_sales_by_genre_df.shape)
+# print(vid_game_sales_by_genre_df.to_string())
+# print(vid_game_sales_by_genre_df.shape)
 
 # reset index of df so 'Genre' can be selected as column
 vid_game_sales_by_genre_df = vid_game_sales_by_genre_df.reset_index()
 
 # rename 'Role-Playing' 'Genre' to RPG to save space on plot
 vid_game_sales_by_genre_df.loc[8, 'Genre'] = 'RPG'
-#print(vid_game_sales_by_genre_df)
+# print(vid_game_sales_by_genre_df)
 
 # create list of 'Genre' values for use on y-axis plot
 y_labels = vid_game_sales_by_genre_df['Genre'].to_list()
-#print(y_labels)
+# print(y_labels)
 
 # create basic plot/chart and adjust y-axis font size using yticks()
 plt.barh(y_labels, vid_game_sales_by_genre_df['Global_Sales'])
 plt.title('Total Video Game Sales by Genre')
 plt.xlabel('Millions Sold')
-plt.yticks(fontsize= 9)
+plt.yticks(fontsize=9)
 plt.show()
 
 # create separate figures for top four selling genres for plotting
@@ -62,18 +63,19 @@ fig4_other = vid_game_sales_by_genre_df[['Genre', 'Other_Sales']]
 complex_graph_labels = fig1_na['Genre'].tolist()
 
 # create total_height variable
-total_height = [sum(x) for x in zip(fig1_na['NA_Sales'].tolist(), fig2_eu['EU_Sales'].tolist(), fig3_jp['JP_Sales'].tolist(), fig4_other['Other_Sales'].tolist())]
+total_height = [sum(x) for x in
+                zip(fig1_na['NA_Sales'].tolist(), fig2_eu['EU_Sales'].tolist(), fig3_jp['JP_Sales'].tolist(),
+                    fig4_other['Other_Sales'].tolist())]
 
-fig1_na = (fig1_na['NA_Sales']/total_height)*100
-fig2_eu = (fig2_eu['EU_Sales']/total_height)*100
-fig3_jp = (fig3_jp['JP_Sales']/total_height)*100
-fig4_other = (fig4_other['Other_Sales']/total_height)*100
+fig1_na = (fig1_na['NA_Sales'] / total_height) * 100
+fig2_eu = (fig2_eu['EU_Sales'] / total_height) * 100
+fig3_jp = (fig3_jp['JP_Sales'] / total_height) * 100
+fig4_other = (fig4_other['Other_Sales'] / total_height) * 100
 
 # create height variables for figures
 fig2_height = fig1_na.tolist()
 fig3_height = [sum(x) for x in zip(fig2_height, fig2_eu.tolist())]
 fig4_height = [sum(x) for x in zip(fig3_height, fig3_jp.tolist())]
-
 
 plt.barh(complex_graph_labels, fig1_na, color='b', label='North American Sales')
 plt.barh(complex_graph_labels, fig2_eu, left=fig2_height, color='g', label='European Sales')
@@ -81,6 +83,6 @@ plt.barh(complex_graph_labels, fig3_jp, left=fig3_height, color='y', label='Japa
 plt.barh(complex_graph_labels, fig4_other, left=fig4_height, color='purple', label='Other Regions Sales')
 plt.grid(axis='x')
 plt.yticks(fontsize=9)
-plt.legend(loc= 'best', fancybox=True, shadow=True, fontsize=9)
+plt.legend(loc='best', fancybox=True, shadow=True, fontsize=9)
 plt.title('% of Video Game Genre Sales by Region')
 plt.show()
--- PyPlot_Plots_Ehlert.py
+++ PyPlot_Plots_Ehlert.py
@@ -15,25 +15,24 @@ plt.rcParams['figure.dpi'] = 450
 
 # read csv file and create new df
 base_climate_df = pd.read_csv("IA_HI_Climate_Dataset.csv")
-#print(base_climate_df.columns)
+# print(base_climate_df.columns)
 
 # create new df with only needed columns from base df
 trimmed_climate_df = base_climate_df[['NAME', 'DATE', 'PRCP', 'TAVG']]
-#print(trimmed_climate_df.dtypes)
+# print(trimmed_climate_df.dtypes)
 
 # remove any records that contain NaN in either 'PRCP' or 'TAVG' columns and create new df variable
 nan_records_drop_df = trimmed_climate_df.dropna(subset=['PRCP', 'TAVG'])
-#print(nan_records_drop_df)
+# print(nan_records_drop_df)
 
 # convert DATE col to datetime datatype and remove records not in month of January
 jan_climate_df = nan_records_drop_df.copy()
 jan_climate_df['DATE'] = pd.to_datetime(jan_climate_df['DATE'])
 jan_climate_df = jan_climate_df[jan_climate_df['DATE'].dt.month == 1]
-#print(jan_climate_df.to_string())
+# print(jan_climate_df.to_string())
 
 # create figures with only capital cities of states (one city per figure)
 fig1_ia = jan_climate_df[jan_climate_df['NAME'] == 'DES MOINES INTERNATIONAL AIRPORT, IA US']
-#print(fig1_ia)
 fig2_hi = jan_climate_df[jan_climate_df['NAME'] == 'HONOLULU INTERNATIONAL AIRPORT, HI US']
 
 # create basic plot showing average temp for locations
@@ -48,7 +47,7 @@ plt.legend()
 plt.show()
 
 # create complex plot using subplots
-fig, axs = plt.subplots(2,1)
+fig, axs = plt.subplots(2, 1)
 
 axs[0].plot(fig1_ia['DATE'].dt.day, fig1_ia['TAVG'], label='Des Moines, IA')
 axs[0].plot(fig2_hi['DATE'].dt.day, fig2_hi['TAVG'], label='Honolulu, HI')
@@ -58,7 +57,7 @@ axs[0].title.set_text('Avg. Temp(F) for January 2023')
 axs[0].axes.set_ylabel('Avg. Temp(F)')
 axs[0].legend()
 axs[0].grid(which='major', linestyle='-', color='#DDDDDD', linewidth=1.0)
-axs[0].grid(which='minor', linestyle=':',color='#EEEEEE', linewidth=0.8)
+axs[0].grid(which='minor', linestyle=':', color='#EEEEEE', linewidth=0.8)
 axs[0].minorticks_on()
 
 axs[1].plot(fig1_ia['DATE'].dt.day, fig1_ia['PRCP'], label='Des Moines, IA', color='g')
@@ -68,6 +67,6 @@ axs[1].axes.set_xlabel('Day of Month')
 axs[1].axes.set_ylabel('Inches of Precipitation')
 axs[1].legend()
 axs[1].grid(which='major', linestyle='-', color='#DDDDDD', linewidth=1.0)
-axs[1].grid(which='minor', linestyle=':',color='#EEEEEE', linewidth=0.8)
+axs[1].grid(which='minor', linestyle=':', color='#EEEEEE', linewidth=0.8)
 axs[1].minorticks_on()
 plt.show()
